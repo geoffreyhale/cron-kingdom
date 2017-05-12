c43b681d03782d6561956bb06aa179e0fc91ad87
Tim Kelleher <tim@timkelleher.com>
2017-05-11 22:07:56 -0700

    Make tick its own service

--- src/CronkdBundle/Command/TickCommand.php
+++ src/CronkdBundle/Command/TickCommand.php
@@ -1,11 +1,7 @@
 <?php
 namespace CronkdBundle\Command;
 
-use CronkdBundle\Entity\Log;
-use CronkdBundle\Entity\Queue;
-use CronkdBundle\Entity\Resource;
 use CronkdBundle\Entity\World;
-use CronkdBundle\Event\WorldTickEvent;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,12 +18,10 @@ class TickCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $eventDispatcher = $this->getContainer()->get('event_dispatcher');
         $em = $this->getContainer()->get('doctrine.orm.default_entity_manager');
-        $kingdomManager = $this->getContainer()->get('cronkd.manager.kingdom');
         $logger = $this->getContainer()->get('logger');
-        $logManager = $this->getContainer()->get('cronkd.manager.log');
         $worldManager = $this->getContainer()->get('cronkd.manager.world');
+        $tickService = $this->getContainer()->get('cronkd.tick');
 
         $worldManager->deactivateExpiringWorlds();
         $worlds = $em->getRepository(World::class)->findByActive(true);
@@ -36,52 +30,7 @@ class TickCommand extends ContainerAwareCommand
 
         /** @var World $world */
         foreach ($worlds as $world) {
-            $logger->info('Starting world ' . $world->getName() . ': tick ' . $world->getTick());
-
-            $queues = $em->getRepository(Queue::class)->findNextByWorld($world);
-            $logger->info('Found ' . count($queues) . ' queues to parse');
-
-            /** @var Queue $queue */
-            foreach ($queues as $queue) {
-                $logger->info('Queue is for Kingdom ' . $queue->getKingdom()->getName() . ' for ' . $queue->getResource()->getName());
-
-                $kingdomResource = $kingdomManager->findOrCreateResource($queue->getKingdom(), $queue->getResource());
-                $kingdomResource->addQuantity($queue->getQuantity());
-                $em->persist($kingdomResource);
-
-                if (0 < $queue->getQuantity()) {
-                    $logManager->createLog(
-                        $queue->getKingdom(),
-                        Log::TYPE_TICK,
-                        $queue->getQuantity() . ' ' . $queue->getResource()->getName() . ' are now available'
-                    );
-                }
-                $logger->info('Adding ' . $queue->getQuantity() . ' ' . $queue->getResource()->getName() . '; New balance is ' . $kingdomResource->getQuantity());
-            }
-
-            $logger->info('Completed queues');
-
-            foreach ($world->getKingdoms() as $kingdom) {
-                if (!$kingdomManager->isAtMaxPopulation($kingdom)) {
-                    $addition = $kingdomManager->incrementPopulation($kingdom);
-                    $logManager->createLog(
-                        $kingdom,
-                        Log::TYPE_TICK,
-                        'Gave birth to ' . $addition . ' ' . Resource::CIVILIAN
-                    );
-                    $logger->info($kingdom->getName() . ' kingdom is not at capacity, adding ' . $addition . ' to population');
-                } else {
-                    $logger->info($kingdom->getName() . ' is at capacity');
-                }
-            }
-
-            $world->addTick();
-            $em->persist($world);
-            $em->flush();
-
-            $event = new WorldTickEvent($world);
-            $eventDispatcher->dispatch('event.world_tick', $event);
-            $logger->info('Completed tick ' . $world->getTick() . ' for world ' . $world->getName());
+            $tickService->performTick($world);
         }
 
         $worldManager->activateUpcomingWorlds();
