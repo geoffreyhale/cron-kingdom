74e26de1d95a301e2e4195f531b822fd27904cff
geoffreyhale <geoffreyhale@gmail.com>
2017-12-15 22:34:58 -0800

    attack form fails if user sends non-numeric quantity of resource

--- src/CronkdBundle/Form/AttackPlanType.php
+++ src/CronkdBundle/Form/AttackPlanType.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -29,7 +29,7 @@ class AttackPlanType extends AbstractType
         /** @var Resource $resource */
         foreach ($options['resources'] as $resource) {
             if ($resource->getAttack() > 0 && $options['kingdomState']->hasAvailableResource($resource->getName())) {
-                $builder->add($resource->getName(), TextType::class, [
+                $builder->add($resource->getName(), NumberType::class, [
                     'required' => true,
                 ]);
             }
