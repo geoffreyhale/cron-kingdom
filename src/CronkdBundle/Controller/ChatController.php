36d135ce8ec09f2e2efb83498040c33f8a2c9719
geoffreyhale <geoffreyhale@gmail.com>
2017-12-25 19:39:09 -0800

    add basic chat

--- src/CronkdBundle/Controller/ChatController.php
+++ src/CronkdBundle/Controller/ChatController.php
@@ -1,27 +1,59 @@
 <?php
 namespace CronkdBundle\Controller;
 
-use CronkdBundle\Entity\Kingdom;
-use CronkdBundle\Entity\World;
+use CronkdBundle\Entity\ChatMessage;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
-class ChatController extends Controller
+/**
+ * @Route("/chat")
+ */
+class ChatController extends CronkdController
 {
     /**
-     * @Route("/chat", name="chat")
+     * @Route("", name="chat")
      * @Template
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $kingdom = $this->extractKingdomFromCurrentUser();
+
+        if (empty($kingdom)) {
+            throw $this->createAccessDeniedException('You must be logged in to a kingdom to access chat!');
+        }
+
+        $message = new ChatMessage();
+        $message->setKingdom($kingdom);
+
+        $form = $this->createFormBuilder($message)
+            ->add('body', TextType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = $form->getData();
+
+            if (!empty($message->getBody())) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($message);
+                $em->flush();
+            }
+
+            return $this->redirect($this->generateUrl('chat'));
+        }
+
         $em = $this->getDoctrine()->getManager();
-        $user = $this->getUser();
-        $world = $em->getRepository(World::class)->findOneBy(['active' => true]);
-        $kingdom = $em->getRepository(Kingdom::class)->findOneByUserWorld($user, $world);
+        $messagesByCreatedAt = $em->getRepository(ChatMessage::class)->findBy([], ['createdAt' => 'ASC']);
 
         return [
-            'kingdom' => $kingdom
+            'form' => $form->createView(),
+            'messages' => $messagesByCreatedAt
         ];
     }
 }
---
+++ src/CronkdBundle/Entity/ChatMessage.php
@@ -0,0 +1,88 @@
+<?php
+namespace CronkdBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="chat_message")
+ * @ORM\Entity
+ */
+class ChatMessage extends BaseEntity
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="body", type="string", length=255)
+     */
+    private $body;
+
+    /**
+     * @var Kingdom
+     *
+     * @ORM\ManyToOne(targetEntity="Kingdom", inversedBy="messages")
+     */
+    private $kingdom;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return ChatMessage
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     * @return ChatMessage
+     */
+    public function setBody(string $body)
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    /**
+     * @return Kingdom
+     */
+    public function getKingdom()
+    {
+        return $this->kingdom;
+    }
+
+    /**
+     * @param Kingdom $kingdom
+     * @return ChatMessage
+     */
+    public function setKingdom(Kingdom $kingdom)
+    {
+        $this->kingdom = $kingdom;
+        return $this;
+    }
+}
